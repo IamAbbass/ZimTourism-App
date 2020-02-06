27407fc19a1b7ec881152a9f1b39213b9a33a9b3
Ghulam Abbass <ghulamabbass.1995@gmail.com>
2020-02-07 01:11:32 +0500

    Updated

---
+++ theafricatour/lib/screen/book_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:theafricatour/common/theme.dart';
+
+class BookScreen extends StatefulWidget {
+  @override
+  _BookScreenState createState() => _BookScreenState();
+}
+
+class _BookScreenState extends State<BookScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: 1000)).then((v) {
+      Navigator.pop(context);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = HotelConceptThemeProvider.get();
+    return Scaffold(
+      body: Container(
+        //color: themeData.accentColor,
+        color: Colors.red,
+        child: Center(
+          child: Text(
+            "Payment API Timeout",
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
